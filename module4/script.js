cea9adab41f0943fac131d9f2e26724f2b3c7d84
abhilaksh-arora <arora.abhilaksh@gmail.com>
2021-10-18 22:15:04 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Jam", "Ram", "Harit", "Aakash", "Jatin", "Rohan", "Mohan", "Shyam", "Laksh", "Abhi"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
